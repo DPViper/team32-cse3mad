76e8f5443dc4c255ef91538d6c160f5a9b06e164
dinhanhduytran <dinhanhduy.tran@gmail.com>
2025-05-11 23:00:19 +1000

    UI of POI

--- app/features/poi/screens/ReviewScreen.tsx
+++ app/features/poi/screens/ReviewScreen.tsx
@@ -1,11 +1,23 @@
 import { useRouter, useLocalSearchParams } from "expo-router";
-import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
 import { useState } from "react";
 import StarRating from "@/app/features/poi/components/StarRating";
 import { submitRating } from "@/app/features/poi/services/submitRatings";
 import { useAuth } from "@/hooks/useAuth";
+import { useTheme } from "@/contexts/ThemeContext";
+import { Button } from "@/components/ui/button";
 
 export default function ReviewScreen() {
+  const theme = useTheme();
+  const styles = createThemedStyles(theme);
   const { id, image } = useLocalSearchParams();
   const router = useRouter();
   const { user } = useAuth();
@@ -39,37 +51,48 @@ export default function ReviewScreen() {
       <StarRating rating={rating} onChange={setRating} />
 
       <TextInput
-        placeholder="Write your review..."
+        placeholder="Write your review here"
+        placeholderTextColor={theme.placeholderText}
         value={text}
         onChangeText={setText}
         multiline
         style={styles.textbox}
       />
-
-      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-        <Text style={styles.buttonText}>Submit Review</Text>
-      </TouchableOpacity>
+      <Button style={styles.button} onPress={handleSubmit}>
+        Submit Review
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, backgroundColor: "#fff" },
-  header: { fontSize: 20, fontWeight: "bold", marginBottom: 12 },
-  image: { height: 200, width: "100%", borderRadius: 8, marginBottom: 16 },
-  textbox: {
-    height: 120,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 10,
-    textAlignVertical: "top",
-    marginBottom: 16,
-  },
-  button: {
-    backgroundColor: "#F58A07",
-    paddingVertical: 12,
-    borderRadius: 8,
-  },
-  buttonText: { textAlign: "center", color: "#fff", fontWeight: "600" },
-});
+function createThemedStyles(theme: any) {
+  return StyleSheet.create({
+    container: { paddingHorizontal: 16, backgroundColor: theme.background },
+    header: {
+      fontSize: 22,
+      marginBottom: 8,
+      alignSelf: "center",
+      color: theme.textDark,
+      fontFamily: "PlusJakartaSansBold",
+    },
+    image: { height: 200, borderRadius: 20, marginBottom: 12 },
+
+    textbox: {
+      height: 120,
+      borderColor: theme.secondary,
+      backgroundColor: theme.inputBackground,
+      borderWidth: 1,
+      borderRadius: 8,
+      padding: 10,
+      textAlignVertical: "top",
+      marginBottom: 16,
+    },
+    button: {
+      paddingVertical: 10,
+      borderRadius: 10,
+      alignSelf: "center",
+      width: "80%",
+    },
+    buttonText: { textAlign: "center", color: "#fff", fontWeight: "600" },
+  });
+}
